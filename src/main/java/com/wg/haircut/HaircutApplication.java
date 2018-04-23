9e70834c264b3d40a0dc8d173d1517c3bd82eb5c
gang594668515 <gang594668515@163.com>
2018-04-23 18:13:46 +0800

    IDEA+SPRING BOOT(WEB)+HELLO(http://localhost:8080/hello)

---
+++ src/main/java/com/wg/haircut/HaircutApplication.java
@@ -0,0 +1,11 @@
+package com.wg.haircut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaircutApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HaircutApplication.class, args);
+    }
+}
